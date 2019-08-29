742c67d305ed9cd0ceceb0f0b5d2c62fef2b4cbe
dneupokoev <dials@mail.ru>
2019-08-29 11:42:15 +0300

    Add files via upload

---
+++ R/dix_write_csv.R
@@ -0,0 +1,32 @@
+#'dix
+#'
+#' @title dix_write_csv
+#'
+#' @description csv-file generation function from parameters
+#'
+#' @return csv-file
+#'
+#' @examples dix_write_csv
+#'
+#' @export
+#'
+#'
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+dix_write_csv <- function(param_in01=NA, param_in02=NA){
+  if ( (!is.na(param_in01)) & (!is.na(param_in02)) ) {
+    for (i in 1:length(param_in01)) {
+      param_in01[[i]] <- iconv(param_in01[[i]], from = "utf-8", to = "windows-1251")
+      param_in01[[i]] <- iconv(param_in01[[i]], from = "utf-8", to = "windows-1251")
+    }
+    write.csv(param_in01, file = param_in02, row.names = FALSE, na="")
+  }
+}
+
+
+
