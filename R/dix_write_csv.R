2dc1ad15d632558244a62ac6686d9c7805d62668
dneupokoev <dials@mail.ru>
2019-08-29 15:13:07 +0300

    Add files via upload

--- R/dix_write_csv.R
+++ R/dix_write_csv.R
@@ -18,15 +18,19 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-dix_write_csv <- function(param_in01=NA, param_in02=NA){
+dix_write_csv_test <- function(param_in01=NA, param_in02=NA, param_in03=NA){
   if ( (!is.na(param_in01)) & (!is.na(param_in02)) ) {
     for (i in 1:length(param_in01)) {
       param_in01[[i]] <- iconv(param_in01[[i]], from = "utf-8", to = "windows-1251")
       param_in01[[i]] <- iconv(param_in01[[i]], from = "utf-8", to = "windows-1251")
-    }
-    write.csv(param_in01, file = param_in02, row.names = FALSE, na="")
+      }
+  
+    if (is.na(param_in03)) {
+      write.csv(param_in01, file = param_in02, row.names = FALSE, na="")
+      }
+  
+    if (param_in03 == 'gz') {
+      write.csv(param_in01, file = gzfile(paste0(param_in02,".",param_in03)), row.names = FALSE, na="")
+      }
   }
 }
-
-
-
