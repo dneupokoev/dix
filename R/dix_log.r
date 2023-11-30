2af5d63f4e15e694fd91fb71096b3690c92d3b36
dneupokoev <dials@mail.ru>
2023-11-30 07:56:18 +0300

    Update dix_log.r

--- R/dix_log.r
+++ R/dix_log.r
@@ -1,12 +1,12 @@
 #'dix
 #'
-#' @title dix_print
+#' @title dix_log
 #'
 #' @description text generation function from transmitted parameters
 #'
 #' @return string
 #'
-#' @examples dix_print
+#' @examples dix_log
 #'
 #' @export
 #'
@@ -18,26 +18,16 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-dix_print <- function(print_in01=NA, print_in02=NA, print_in03=NA){
-  txt_f_print <- paste( as.character(Sys.time()), "|", as.character(print_in01) )
-
-  if (!is.na(print_in02)) {
-    txt_f_print <- paste( txt_f_print, "|", as.character(print_in02) )
-  }
-
-  if (!is.na(print_in03)) {
-    txt_f_print <- paste( txt_f_print, "|", as.character(print_in03) )
-  }
-
-  print(txt_f_print, quote=FALSE)
-}
-
 dix_log <- function(print_in01=NA, print_in02=NA, print_in03=NA){
   txt_f_print <- paste0(as.character(Sys.time()), ' | ', as.character(print_in01))
   txt_f_memory <- paste0(' (', round(memory.size(), 0), 'MB)')
   # 
   if (!is.na(print_in02)) {
-    txt_f_print <- paste0(txt_f_print, ' | ', as.character(print_in02), txt_f_memory)
+    if (as.character(print_in02) %in% c('begin', 'end')) {
+      txt_f_print <- paste0(txt_f_print, ' | ', as.character(print_in02))
+    } else {
+      txt_f_print <- paste0(txt_f_print, ' | ', as.character(print_in02), txt_f_memory)
+    }
   }
   # 
   if (!is.na(print_in03)) {
