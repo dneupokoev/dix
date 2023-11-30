87254d5af501c26203f427e7998c92a499edbd4a
dneupokoev <dials@mail.ru>
2023-11-30 14:41:16 +0300

    1.3

--- R/dix_log.r
+++ R/dix_log.r
@@ -23,7 +23,7 @@ dix_log <- function(print_in01=NA, print_in02=NA, print_in03=NA){
   txt_f_memory <- paste0(' (', round(memory.size(), 0), ' MB)')
   # 
   if (!is.na(print_in02)) {
-    if (as.character(print_in02) %in% c('begin', 'end')) {
+    if (as.character(print_in02) %in% c('begin', 'end', 'WARNING', 'ERROR', 'INFO')) {
       txt_f_print <- paste0(txt_f_print, ' | ', as.character(print_in02))
     } else {
       txt_f_print <- paste0(txt_f_print, ' | ', as.character(print_in02), txt_f_memory)
