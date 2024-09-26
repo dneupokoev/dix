eda746b941b03136572fc495ed953a7903a35dcc
dneupokoev <dials@mail.ru>
2024-09-26 12:06:57 +0300

    Update dix_eval_timeout.R

--- R/dix_eval_timeout.R
+++ R/dix_eval_timeout.R
@@ -26,16 +26,16 @@ dix_eval_timeout <- function(time_limit_sec = 60, count_attemps = 10, eval_exec 
   dv_return <- 'FAIL'
   # count_attemps - Количество попыток, если что-то пошло не так
   dv_stat_count_attemps <- 0
-  # Задаем параметры с указанием лимитов:
-  setTimeLimit(cpu = time_limit_sec, elapsed = time_limit_sec, transient = TRUE)
-  on.exit({
-    setTimeLimit(cpu = Inf, elapsed = Inf, transient = FALSE)
-  })
   while (count_attemps > 0) {
     # Тело скрипта:
     tryCatch(
       expr = {
         dv_stat_count_attemps <- dv_stat_count_attemps + 1
+        # Задаем параметры с указанием лимитов:
+        setTimeLimit(cpu = time_limit_sec, elapsed = time_limit_sec, transient = TRUE)
+        on.exit({
+          setTimeLimit(cpu = Inf, elapsed = Inf, transient = FALSE)
+        })
         # Здесь выполняем нужное:
         dv_return <- eval(parse(text=eval_exec))
         # Если дошли до этого места, то всё норм и прерываем цикл:
@@ -43,11 +43,12 @@ dix_eval_timeout <- function(time_limit_sec = 60, count_attemps = 10, eval_exec 
       },
       error = function(e) {
         dv_return <<- paste0('ERROR *** ', e)
-        # Делаем небольшую паузу (случайную от 3 до 30 секунд):
+        # Задаем параметры с указанием лимитов:
         setTimeLimit(cpu = 60, elapsed = 60, transient = TRUE)
         on.exit({
           setTimeLimit(cpu = Inf, elapsed = Inf, transient = FALSE)
         })
+        # Делаем небольшую паузу (случайную от 3 до 30 секунд):
         Sys.sleep(sample(3:30, 1))
       },
       finally = {
@@ -56,7 +57,7 @@ dix_eval_timeout <- function(time_limit_sec = 60, count_attemps = 10, eval_exec 
     )
   }
   # Отображаем количество попыток выполнения eval (если не с первой):
-  if (dv_stat_count_attemps > 1) {
+  if (dv_stat_count_attemps > 0) {
     dix_log(paste0(script_name, ' - Number of attempts dix_eval_timeout = ', dv_stat_count_attemps))
   }
   return(dv_return)
